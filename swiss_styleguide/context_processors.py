0e1ee43ff093f6e3fbe96f575930faf79f6d3871
Stefan Heinemann <stefan.heinemann@codedump.ch>
2017-05-12 13:54:40 +0200

    Correct the department title

--- swiss_styleguide/context_processors.py
+++ swiss_styleguide/context_processors.py
@@ -3,5 +3,6 @@ from django.conf import settings
 
 def auth(request):
     return {
-        'app_title': settings.SWISS_STYLEGUIDE['app_title']
+        'app_title': settings.SWISS_STYLEGUIDE['app_title'],
+        'department_title':  settings.SWISS_STYLEGUIDE['department_title']
     }
